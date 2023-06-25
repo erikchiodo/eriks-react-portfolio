e7baa4a9bfe4cd4ad3e91518d76fe77fa5c9bbb0
erikchiodo <erikchiodo@gmail.com>
2023-06-25 17:34:15 -0400

    Updated Contact, Resume Content and added Resume.PDF

--- eriks-portfolio/src/components/pages/Contact.jsx
+++ eriks-portfolio/src/components/pages/Contact.jsx
@@ -1,12 +1,22 @@
+//TODO: Create input for Name, Email Address, and Message
+//TODO: For Name, Email Address, and Message, if user clicks in box and clicks out without filling in information, produce error saying "X is required". Clear error once user starts entering information
+//TODO: Add Regex Validation that checks for valid email input. If not valid input, produce error saying "Enter Valid Email"
+
 import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        This is Contact page
-      </p>
-    </div>
+  <form>
+    <label htmlfor="name">Name:</label>
+    <input type="text" id="name" name="name" required/>
+
+    <label htmlfor="email">Email:</label>
+    <input type="email" id="email" name="email" required/>
+
+    <label htmlfor="message">Message:</label>
+    <textarea id="message" name="message" rows="4" required></textarea>
+
+    <input type="submit" value="Submit"/>
+  </form>
   );
 }
--- eriks-portfolio/src/components/pages/Resume.jsx
+++ eriks-portfolio/src/components/pages/Resume.jsx
@@ -1,3 +1,5 @@
+//TODO: Fix resume link. Currently does not redirect to PDF of Resume.PDF (in asset folder)
+
 import React from 'react';
 
 export default function Resume() {
@@ -5,9 +7,32 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <p>
-        Download my<span><a href="path/to/your/resume.pdf">Resume</a></span>
-        resume
+        Download my{" "}
+        <span>
+          <a href="../assets/Resume.pdf" className="resume-download">
+            Resume
+          </a>
+        </span>
       </p>
+      <h2>Front-end Proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>BootStrap</li>
+      </ul>
+      <h2>Back-end Proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>GraphQL</li>
+        <li>MongoDB</li>
+      </ul>
     </div>
   );
 }
