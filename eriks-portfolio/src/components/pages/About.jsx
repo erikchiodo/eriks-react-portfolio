d1e6913a5b20af5817ffdb5ab948ebfaca165236
erikchiodo <erikchiodo@gmail.com>
2023-06-25 20:22:34 -0400

    Added self image and re-installed packages (to resolve react errors)

--- eriks-portfolio/src/components/pages/About.jsx
+++ eriks-portfolio/src/components/pages/About.jsx
@@ -4,8 +4,15 @@ export default function About() {
   return (
     <div>
       <h1>About Me</h1>
+      <img src="../assets/Self_Image.png" alt="Self-Image"/>
       <p>
-       My name is Erik Chiodo. I work as Product Manager at JP Morgan Chase as a Product Manager where I mananged a team called Coda, which manages reference data and CRM integrations. I'm currently enrolled in Columbia's Full-Stack Bootcamp where I've learned to HTML, CSS, & Javascript. I also have experience with Node.js, Express.js, and React. After I complete this course, I'm looking to build on my newly-acquired technical abilities by learning Java.
+        My name is Erik Chiodo. I work as Product Manager at JP Morgan Chase as
+        a Product Manager where I mananged a team called Coda, which manages
+        reference data and CRM integrations. I'm currently enrolled in
+        Columbia's Full-Stack Bootcamp where I've learned to HTML, CSS, &
+        Javascript. I also have experience with Node.js, Express.js, and React.
+        After I complete this course, I'm looking to build on my newly-acquired
+        technical abilities by learning Java.
       </p>
     </div>
   );
